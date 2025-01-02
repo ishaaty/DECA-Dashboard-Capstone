756b3113e56b0edcaa8f3a2249b07a1c8d08ad43
J-Tell <fairyq007@hotmail.com>
2025-01-02 09:56:04 -0500

    added a user button that will soon display user information

---
+++ rapid-prototype/src/pages/User/User.js
@@ -0,0 +1,11 @@
+import './User.css';
+import Header from '../../components/Header/Header'
+
+export default function User() {
+    return (
+        <>
+            <Header />
+            <h2>User</h2>
+        </>
+    )
+}
