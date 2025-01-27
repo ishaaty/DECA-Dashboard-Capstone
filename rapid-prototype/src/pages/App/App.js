d166d4b9e308f5056a993e5a20922a32d0e9689a
Isha Tyagi <ishatyagi888@gmail.com>
2025-01-27 10:15:33 -0500

    fixed routing of view add events

--- rapid-prototype/src/pages/App/App.js
+++ rapid-prototype/src/pages/App/App.js
@@ -16,7 +16,7 @@ import Fundraisers from "../Fundraisers/Fundraisers"
 import Participants from "../Participants/Participants"
 import Resources from "../Resources/Resources"
 import FundraiserApproval from "../Fundraisers/FundraiserApproval/FundraiserApproval"
-import ViewAddEvents from '../ViewAddEvents/ViewAddEvents';
+import ViewAddEvents from '../Competitions/ViewAddEvents/ViewAddEvents';
 import ViewRequesters from '../ViewRequesters/ViewRequesters';
 import Roommates from '../Roommates/Roommates';
 import StudentProfile from '../StudentProfile/StudentProfile';
@@ -40,8 +40,8 @@ export default function App() {
           {/* pages that DO change by userRole */}
           <Route path="/home" element={<Home userRole={userType} />}/>
 
-          <Route path="/competitions" element={<Competitions/>}/>
-          <Route path="/viewaddevents" element={<ViewAddEvents/>}/>
+          <Route path="/competitions" element={<Competitions userRole={userType} />}/>
+          <Route path="/viewaddevents" element={<ViewAddEvents userRole={userType}/>}/>
           <Route path="/viewrequesters" element={<ViewRequesters/>}/>
           <Route path="/roommates" element={<Roommates/>}/>
           <Route path="/studentprofile" element={<StudentProfile/>}/>
