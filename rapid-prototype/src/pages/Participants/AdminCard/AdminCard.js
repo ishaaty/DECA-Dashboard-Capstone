b667dfd26f9ffbb0cf835225c89c1454914d457a
Julia <jewelst2014@gmail.com>
2025-01-30 10:35:10 -0500

    Made board member clickable on participants

--- rapid-prototype/src/pages/Participants/AdminCard/AdminCard.js
+++ rapid-prototype/src/pages/Participants/AdminCard/AdminCard.js
@@ -1,13 +1,24 @@
 import './AdminCard.css';
 
 export default function AdminCard(props) {
-    return (
-        <>
+    if (props.role === "admin") {
+        return (
             <div class="accontainer">
-                <h4 id="name">{props.name}</h4>
-                <p class="data">{props.email}</p>
+            <h4 id="name">{`${props.firstName} ${props.lastName}`}</h4>
+            <p class="data">{props.email}</p>
             </div>
-
-        </>
-    )
+        )
+    } else {
+        return (
+            <>
+                <div class="accontainer">
+                <a className="partanchor"href={`/participantdetails?userFirst=${props.firstName}&userLast=${props.lastName}`}>
+                    <h4 className="partname">{`${props.firstName} ${props.lastName}`}</h4>
+                </a>
+                    <p class="data">{props.email}</p>
+                </div>
+    
+            </>
+        )
+    }
 }
