61a8c8de703a5bd87791c89c1dce6991f0880015
Julia <jewelst2014@gmail.com>
2025-03-25 10:29:38 -0400

    Competitions styling

--- src/pages/Competitions/CompTypeCard/CompTypeCard.js
+++ src/pages/Competitions/CompTypeCard/CompTypeCard.js
@@ -2,7 +2,7 @@ import './CompTypeCard.css';
 
 export default function CompTypeCard(props) {
     return (
-        <button className="compCard" style={{ backgroundColor: props.color}}>
+        <button className="compCard" style={{ backgroundColor: props.color, borderColor: props.color }}>
             <h1>{props.name}</h1>
         </button>
     )
