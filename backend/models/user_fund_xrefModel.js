cb74e2459e504420db4b20a9fae4ee815a8622cd
matchka <myung.yaejun@gmail.com>
2025-05-10 13:52:56 -0400

    fund

--- backend/models/user_fund_xrefModel.js
+++ backend/models/user_fund_xrefModel.js
@@ -18,7 +18,7 @@ module.exports = (sequelize) => {
     status: {
       type: DataTypes.STRING(45),
       allowNull: false,
-      defaultValue: 'pending',
+      defaultValue: 'default',
     }
   }, {
     tableName: 'user_fund_xref',
