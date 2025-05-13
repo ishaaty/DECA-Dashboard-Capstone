b9c9ba9b27a9ed5e651972e7cb4d6d9b15d6cdee
matchka <myung.yaejun@gmail.com>
2025-05-12 22:34:54 -0400

    changed fundheader again

--- src/pages/Fundraisers/Fundraisers.js
+++ src/pages/Fundraisers/Fundraisers.js
@@ -219,7 +219,7 @@ if (userRole === "admin") {
         <Header />
         <Menu />
 
-        <div id = "fundheader">My Fundraisers</div>
+        <div id = "fundheader">Fundraisers</div>
     
         <div className ="funds">
           {fundraisers.length > 0 ? (
