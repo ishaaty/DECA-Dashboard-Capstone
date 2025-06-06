2a1f0448f4614596f8e047c39cabb34604889e2c
ishaaty <ishatyagi888@gmail.com>
2025-06-06 19:03:14 -0400

    commented out local db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,17 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config(); // No path needed
 
-// Replace with your actual database credentials
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,   // Change to your database host
-  dialect: 'mysql',    // Or 'postgres', 'sqlite', 'mariadb', etc.
-  pool: {
-    max: 200,  // Maximum number of connections
-    min: 0,   // Minimum number of connections
-    acquire: 30000,  // Max time (in ms) before a connection is considered unavailable
-    idle: 10000       // Max time (in ms) that a connection can be idle before being released
-  }
-});
+// // Replace with your actual database credentials
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,   // Change to your database host
+//   dialect: 'mysql',    // Or 'postgres', 'sqlite', 'mariadb', etc.
+//   pool: {
+//     max: 200,  // Maximum number of connections
+//     min: 0,   // Minimum number of connections
+//     acquire: 30000,  // Max time (in ms) before a connection is considered unavailable
+//     idle: 10000       // Max time (in ms) that a connection can be idle before being released
+//   }
+// });
 
 // Railway DB
 const railwaySequelize = new Sequelize(process.env.RAILWAY_DB_URL, {
@@ -20,9 +20,13 @@ const railwaySequelize = new Sequelize(process.env.RAILWAY_DB_URL, {
 });
 
 
+// module.exports = {
+//   sequelize,
+//   railwaySequelize,
+// };
+
 module.exports = {
-  sequelize,
-  railwaySequelize,
+  railwaySequelize
 };
 
 // module.exports = sequelize;
