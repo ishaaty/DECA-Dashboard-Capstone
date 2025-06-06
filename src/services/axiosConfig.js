7963f863773376be27a66e3dabec2a1efb205120
ishaaty <ishatyagi888@gmail.com>
2025-06-06 19:45:35 -0400

    updated axios config

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8081',
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+  // baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8081',
   headers: { 'Content-Type': 'application/json' },
 });
 
